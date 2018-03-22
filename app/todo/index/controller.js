e2a1684d0c862a42ccb37864448f33756e342486
Esteban Cairol <ecairol@gmail.com>
2018-03-22 13:00:33 -0600

    Search functionality

--- app/todo/index/controller.js
+++ app/todo/index/controller.js
@@ -1,6 +1,28 @@
 import Ember from 'ember';
+import { debug } from '@ember/debug';
 
 export default Ember.Controller.extend({
+  queryParams: ['search'], 
+  search: '',
+
+  filtered: Ember.computed('search', 'model', function() {
+    const items = this.get('model'); 
+    const search = this.get('search').toLowerCase(); 
+ 
+    if (Ember.isEmpty(search)) { 
+      return items; 
+    } 
+ 
+    return items.filter(function(item) { 
+      const name = (item.get('name') || '').toLowerCase(); 
+      const id = item.id.toLowerCase();
+      if (name.match(search) || id.match(search)) { 
+        return true;
+      } 
+      return false; 
+    }) 
+  }), 
+
   actions: {
     newItem(itemName) {
       let newItem = this.store.createRecord('list-item', {
@@ -35,6 +57,10 @@ export default Ember.Controller.extend({
           record.set('done', true);
           record.save();
         });
+    },
+
+    search(keywords) {
+      this.set('search', keywords);
     }
   }
 });
