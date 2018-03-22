8093a5318e2cf594458ca8bf2f58ac99fb949e10
Esteban Cairol <ecairol@gmail.com>
2018-03-22 09:21:35 -0600

    Update for Mirage Factory

--- mirage/factories/list-item.js
+++ mirage/factories/list-item.js
@@ -1,9 +1,13 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
+const items = ["Milk", "Apples", "Avocado", "Chips", "Beer", "Oranges", "Cereal", "Soap"];
+
 export default Factory.extend({
   id(i) {
     return i+1;
   },
   done: false,
-  name: faker.lorem.word
+  name() {
+    return items[Math.floor((Math.random()*items.length))];
+  }
 });
